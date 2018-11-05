c6e20943f78c23caee2838240484a02178100a9a
Andrew Kostrenko <nordork@gmail.com>
2018-11-05 11:45:46 +0300

    ДЗ - 3

--- hw03_normal.py
+++ hw03_normal.py
@@ -1,64 +1,65 @@
-# # Задание-1:
-# # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
-# # Первыми элементами ряда считать цифры 1 1
-#
-# def fibonacci(n, m):
-#     """
-#     Возвращает список элементов с n по m из ряда Фибоначчи
-#     :param n: номер начального элемента
-#     :param m: номер последнего элемента
-#     :return: list
-#     """
-#
-#     f_nums = [1, 1]
-#
-#     while len(f_nums) < m:
-#         f_nums.append(f_nums[-2] + f_nums[-1])
-#     return f_nums[n-1:m]
+# Задание-1:
+# Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
+# Первыми элементами ряда считать цифры 1 1
 
 
-# # Задача-2:
-# # Напишите функцию, сортирующую принимаемый список по возрастанию.
-# # Для сортировки используйте любой алгоритм (например пузырьковый).
-# # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
-#
-#
-# def sort_to_max(origin_list):
-#     """
-#     Сортировка значений списка по возрастанию
-#     :param origin_list: сортируемый список
-#     :return: list
-#     """
-#     sorted_list = []
-#     while len(origin_list) > 0:
-#         n = min(origin_list)
-#         sorted_list.append(n)
-#         origin_list.remove(n)
-#     return sorted_list
-#
-#
-# sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+def fibonacci(n, m):
+    """
+    Возвращает список элементов с n по m из ряда Фибоначчи
+    :param n: номер начального элемента
+    :param m: номер последнего элемента
+    :return: list
+    """
 
-# # Задача-3:
-# # Напишите собственную реализацию стандартной функции filter.
-# # Разумеется, внутри нельзя использовать саму функцию filter.
-#
-#
-# def my_filter(condition, data):
-#     """
-#     Фильтрует переданный список по условию
-#     :param condition: функция(условие) проверки элементов списка (должна возвращать True или False)
-#     :param data: список фильтруемых значений
-#     :return: list
-#     """
-#     filtered_data = []
-#     for i in data:
-#         if condition(i):
-#             filtered_data.append(i)
-#     return filtered_data
-#
-#
-# print(my_filter(lambda x: x > 0, [-2, 0, 6, -1, 10]))
+    f_nums = [1, 1]
+
+    while len(f_nums) < m:
+        f_nums.append(f_nums[-2] + f_nums[-1])
+    return f_nums[n-1:m]
+
+
+# Задача-2:
+# Напишите функцию, сортирующую принимаемый список по возрастанию.
+# Для сортировки используйте любой алгоритм (например пузырьковый).
+# Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
+
+
+def sort_to_max(origin_list):
+    """
+    Сортировка значений списка по возрастанию
+    :param origin_list: сортируемый список
+    :return: list
+    """
+    sorted_list = []
+    while len(origin_list) > 0:
+        n = min(origin_list)
+        sorted_list.append(n)
+        origin_list.remove(n)
+    return sorted_list
+
+
+sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+
+# Задача-3:
+# Напишите собственную реализацию стандартной функции filter.
+# Разумеется, внутри нельзя использовать саму функцию filter.
+
+
+def my_filter(condition, data):
+    """
+    Фильтрует переданный список по условию
+    :param condition: функция(условие) проверки элементов списка (должна возвращать True или False)
+    :param data: список фильтруемых значений
+    :return: list
+    """
+    filtered_data = []
+    for i in data:
+        if condition(i):
+            filtered_data.append(i)
+    return filtered_data
+
+
+print(my_filter(lambda x: x > 0, [-2, 0, 6, -1, 10]))
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
@@ -66,8 +67,6 @@
 
 
 from math import sqrt
-
-
 def is_parallelogram(a, b, c, d):
     """
     Определение, является ли точки вершинами параллерограмма
@@ -88,11 +87,4 @@ def is_parallelogram(a, b, c, d):
 
     return var1 or var2
 
-
-A1 = (1, 3)
-A2 = (4, 7)
-A3 = (2, 8)
-A4 = (-1, 4)
-
-print(is_parallelogram(A1, A2, A3, A4))
 
