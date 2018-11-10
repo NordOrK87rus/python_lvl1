ea9e38d14064ec40e496123f574e47a463b5ddd0
nordork <nordork@gmail.com>
2018-11-10 23:23:45 +0300

    ДЗ05 - normal ready

--- hw05_easy.py
+++ hw05_easy.py
@@ -9,54 +9,59 @@ from re import compile
 # И второй скрипт, удаляющий эти папки.
 
 
-
 def make_dir(dir_name):
     """
-    Функция создающая директорию
-    :param dir_name: абсолютный путь к создаваемой директории
+    Функция создающая папку
+    :param dir_name: абсолютный путь к создаваемой папки
     """
     try:
         mkdir(dir_name)
-        print(f"Директория {dir_name} создана успешно.")
+        print(f" -> Папка \"{dir_name}\" создана успешно.")
     except FileExistsError:
-        print(f"Директория {dir_name} уже существует!")
+        print(f" -> Папка \"{dir_name}\" уже существует!")
 
 
 def del_dir(dir_name):
     """
-    Функция удаляющая указанную директорию
-    :param dir_name: абсолютный путь к удаляемлй директории
+    Функция удаляющая указанную папку
+    :param dir_name: абсолютный путь к удаляемлй папки
     """
     try:
         rmdir(dir_name)
-        print(f"Директория {dir_name} удалена успешно.")
+        print(f" -> Папка \"{dir_name}\" удалена успешно.")
     except FileNotFoundError:
-        print(f"Директория {dir_name} не найдена!")
+        print(f" -> Невозможно удалить \"{dir_name}\". Папка не найдена!")
+    except OSError as e:
+        print(f" -> Невозможно удалить \"{dir_name}\". {e.strerror}")
 
 
-print("Задача-1: Создание директорий:")
-for i in range(9):
-    make_dir(os_path.join(os_path.dirname(argv[0]), f"dir_{i+1}"))
+def list_of_folders():
+    return filter(os_path.isdir, os_listdir(getcwd()))
 
-print("\nЗадача-1: Удаление директорий:")
-regex = compile(r'dir_\d')
-for i in filter(regex.match, os_listdir(os_path.dirname(argv[0]))):
-    del_dir(os_path.abspath(i))
 
-# Задача-2:
-# Напишите скрипт, отображающий папки текущей директории.
-print("\nЗадача-2: Вывод папок текущей директории:")
-print(f"Текущая директория: \"{getcwd()}\"содержит следующие папки:\n ", "\n ".join(filter(os_path.isdir,
-                                                                                           os_listdir(getcwd()))))
+# Небольшой чит, нужен для корректной работы импорта функций в hw05_normal.py
+if __name__ == "__main__":
+    print("Задача-1: Создание директорий:")
+    for i in range(9):
+        make_dir(os_path.join(os_path.dirname(argv[0]), f"dir_{i+1}"))
 
-# Задача-3:
-# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-print("\nЗадача-3: Создание копии файла, из которого запущен данный скрипт: ")
-with open(argv[0], 'r') as s:
-    # Разбираем имя файла скрипта на имя и расширение, для дальнейшего формирования нового имени
-    fn, fe = os_path.splitext(os_path.basename(s.name))
-    print(f"{os_path.basename(s.name)} -> {fn}_copy{fe}")
-    # Создаём копию файла скрипта
-    with open(os_path.join(os_path.dirname(s.name), f"{fn}_copy{fe}"), 'w') as d:
-        d.write(s.read())
+    print("\nЗадача-1: Удаление директорий:")
+    regex = compile(r'dir_\d')
+    for i in filter(regex.match, os_listdir(os_path.dirname(argv[0]))):
+        del_dir(os_path.abspath(i))
 
+    # Задача-2:
+    # Напишите скрипт, отображающий папки текущей директории.
+    print("\nЗадача-2: Вывод папок текущей директории:")
+    print(f"Текущая директория: \"{getcwd()}\"содержит следующие папки:\n ", "\n ".join(list_of_folders()))
+
+    # Задача-3:
+    # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+    print("\nЗадача-3: Создание копии файла, из которого запущен данный скрипт: ")
+    with open(argv[0], 'r') as s:
+        # Разбираем имя файла скрипта на имя и расширение, для дальнейшего формирования нового имени
+        fn, fe = os_path.splitext(os_path.basename(s.name))
+        print(f"{os_path.basename(s.name)} -> {fn}_copy{fe}")
+        # Создаём копию файла скрипта
+        with open(os_path.join(os_path.dirname(s.name), f"{fn}_copy{fe}"), 'w') as d:
+            d.write(s.read())
--- hw05_normal.py
+++ hw05_normal.py
@@ -14,42 +14,55 @@
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
 
-import os
+from os import getcwd, chdir, path as os_path, listdir as os_listdir
+from sys import argv
+from hw05_easy import del_dir, make_dir, list_of_folders
+
+print(f"sys.argv = {argv}\n")
+
+cmd_list = [(1, 'Перейти в папку'),
+            (2, 'Просмотреть содержимое текущей папки'),
+            (3, 'Удалить папку'),
+            (4, 'Создать папку')]
 
 
-def f_help():
-    print("\nДоступны следующие команды:")
-    for c, d in [('help', 'получение справки'), ('cd', 'перейти в папку'),
-                 ('ls', 'вывести содержимое текущей папки'), ('rmd', 'удалить папку'),
-                 ('mkd', 'создать папку'), ('exit', 'выход из программы')]:
-        print("{0:>5} - {1}".format(c, d))
-    print()
-
-
-def f_ls():
-    return os.listdir(os.getcwd())
-
-
-def f_cd():
-    d = input("Введите имя директории для перехода: ")
-    d = d.strip()
-    if d in f_ls():
-        os.chdir(d)
-        print(f"Успешно перешел в директорию {d}")
-    else:
-        print(f"Невозможно перейти в директорию {d}, так как её не существует.")
-
-
-while True:
-    in_cmd = input("Введите команду или help для вывода помощи: ")
-    in_cmd = in_cmd.strip()
-    if in_cmd == 'exit':
-        break
-    elif in_cmd == 'help':
-        f_help()
-    elif in_cmd == 'cd':
-        f_cd()
-    elif in_cmd == 'ls':
-        for i in f_ls():
+def do_cmd(cmd):
+    """
+    Обработка команд пользователя
+    :param cmd: введённая пользователем команда
+    """
+    if cmd == "1":
+        d = input(" -> Введите имя папки для перехода: ")
+        d = d.strip()
+        if d in list_of_folders() or d == os_path.pardir:
+            chdir(d)
+            print(f" -> Успешно перешел в папку {d}")
+        else:
+            print(f" -> Невозможно перейти в папку {d}, так как её не существует.")
+    elif cmd == "2":
+        for i in os_listdir(getcwd()):
             print(" ", i)
+    elif cmd == "3":
+        d = input(" -> Введите имя папки для удаления: ")
+        d = d.strip()
+        del_dir(d.strip())
+    elif cmd == "4":
+        d = input(" -> Введите имя создаваемой папки: ")
+        d = d.strip()
+        make_dir(d.strip())
+    else:
+        print(f"Команда \"{cmd}\" недопустима.")
 
+
+if len(argv) > 1:
+    # Выполняет команду переданную аргументом при запуске утилиты
+    do_cmd(argv[1].strip())
+else:
+    # Если небыло передано аргументов, то утилита просит пользователя ввести команду
+    # и будет выполняться пока пользователь не ведёт exit
+    print(f"\nВыберите действие:")
+    for n, d in cmd_list:
+        print("{0}. {1}".format(n, d))
+
+    in_cmd = input(f":> ")
+    do_cmd(in_cmd.strip())
