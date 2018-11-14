32dfb22f6476dbb7510267618e5428403dd037d0
nordork <nordork@gmail.com>
2018-11-14 16:20:16 +0300

    ДЗ06 - hard ready

--- hw06_hard.py
+++ hw06_hard.py
@@ -11,3 +11,38 @@
 # Реализуйте классы сотрудников так, чтобы на вход функции-конструктора
 # каждый работник получал строку из файла
 
+
+class Worker(object):
+    def __init__(self, data):
+        self.name, self.surename, self.zp, self.rank, self.norm = data.split()
+        self.zp = int(self.zp)
+        self.norm = int(self.norm)
+
+    @property
+    def full_name(self):
+        return f"{self.name.title()} {self.surename.title()}"
+
+    def calc_zp(self, wh):
+        wh = int(wh)
+        if wh < self.norm:
+            return int((self.zp / self.norm) * wh)
+        elif wh > self.norm:
+            return self.zp + int(((self.zp / self.norm) * 2) * (wh - self.norm))
+        else:
+            return self.zp
+
+
+workers_list = []
+with open("data/workers", "r") as wf:
+    for w in wf.readlines()[1:]:
+        workers_list.append(Worker(w))
+
+with open("data/hours_of") as hours:
+    for hof in hours.readlines()[1:]:
+        n, s, hf = hof.split()
+        for w in workers_list:
+            if w.name == n and w.surename == s:
+                print(f"{w.full_name} отработано {hf} часов, зарплата {w.calc_zp(hf)}")
+                break
+        else:
+            print(f"Работник {n.title()} {s.title()} не найден.")
